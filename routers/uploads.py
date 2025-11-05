859d651de2b6ac2e0ef04c1d6973f2334adc9073
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-11-05 11:16:18 -1000

    Update uploads.py

--- routers/uploads.py
+++ routers/uploads.py
@@ -8,7 +8,7 @@ router = APIRouter()
 # --- AWS CONFIGURATION ---
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION", "us-west-2")
+AWS_REGION = os.getenv("AWS_REGION", "us-east-2")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 
 if not all([AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_BUCKET_NAME]):
