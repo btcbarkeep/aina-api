f2befac0477bf07791520bb5d580974d915839ee
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-11-06 22:58:19 -1000

    Update events.py

--- src/routers/events.py
+++ src/routers/events.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from src.database import get_session
 from src.models import Event
 
-router = APIRouter(prefix="Events"])
+router = APIRouter(tags=["Events"])
 
 # 🟢 CREATE event
 @router.post("/", response_model=Event)
