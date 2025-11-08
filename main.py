a642d62615864b9084065b9e2b9b94a01ba9e62c
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-11-08 13:47:14 -1000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+sys.path.append(os.path.dirname(__file__))
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
