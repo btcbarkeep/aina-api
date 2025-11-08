b9de11956a6fb89c87f8ffad8d9498839b4745b1
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-11-08 13:44:47 -1000

    Update dependencies.py

--- src/dependencies.py
+++ src/dependencies.py
@@ -18,7 +18,7 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
     """
     token = credentials.credentials
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
         username: str = payload.get("sub")
         role: str = payload.get("role", "user")
 
