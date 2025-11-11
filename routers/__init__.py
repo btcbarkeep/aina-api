777d6702e9ff86a04129870e2e98036054fbef5c
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-11-11 13:23:21 -1000

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,6 +1,6 @@
 # routers/__init__.py
 from fastapi import APIRouter
-from . import auth, buildings, events, uploads, documents, health
+from . import auth, buildings, events, uploads, sync, documents, health
 
 api_router = APIRouter()
 
@@ -9,6 +9,7 @@ api_router.include_router(auth.router)
 api_router.include_router(buildings.router)
 api_router.include_router(events.router)
 api_router.include_router(uploads.router)
+api_router.include_router(sync.router)
 api_router.include_router(documents.router)
 api_router.include_router(health.router)
 
