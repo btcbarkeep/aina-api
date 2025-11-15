10b1b1d75bf7b0ce353227748b50244568a2f253
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-11-14 16:03:37 -1000

    Create auth.py

---
+++ models/auth.py
@@ -0,0 +1,10 @@
+# models/auth.py
+from sqlmodel import SQLModel
+
+class LoginRequest(SQLModel):
+    username: str
+    password: str
+
+class TokenResponse(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
